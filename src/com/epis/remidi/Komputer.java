5d7c0c511beb0ee34278ffa2f3a0109debeade24
Epis Kuswono <episkuswono@gmail.com>
2018-11-04 12:38:26 +0700

    class Cpu untuk menampilkan specCpu

---
+++ src/com/epis/remidi/Komputer.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.epis.remidi;
+
+/**
+ *
+ * @author Epis Kuswono
+ */
+public class Komputer {
+    private String kode;
+    private int harga;
+    
+}
