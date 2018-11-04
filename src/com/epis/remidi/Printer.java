c1c2fdd03a0ff1e3bf4b8123c18719310346013a
Epis Kuswono <episkuswono@gmail.com>
2018-11-04 12:54:58 +0700

    di class Printer ada dua atribut dan buat sebuah construktor berparameter

---
+++ src/com/epis/remidi/Printer.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.epis.remidi;
+
+/**
+ *
+ * @author Epis Kuswono
+ */
+public class Printer {
+    private String merk, tipe;
+
+    public Printer(String merk, String tipe) {
+        this.merk = merk;
+        this.tipe = tipe;
+    }
+    
+}
